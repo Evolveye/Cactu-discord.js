f0c1ab3042e71e737805f0d5444bcacba2a4bf07
Evolveye <stolarski.paw@gmail.com>
2020-06-27 21:35:19 +0200

    Modules reader

--- index.js
+++ index.js
@@ -2,31 +2,29 @@ import Discord from 'discord.js'
 import fs from 'fs'
 
 import Filters from './Filters.js'
-import Commands from './Commands.js'
+import Commands from './Chat.js'
 import Logger from "./Logger.js"
 
 if (!fs.existsSync( `./guilds_modules/` )) fs.mkdirSync( `./guilds_modules/` )
 
 export const LoggerClass = Logger
-export class GuildData {
-  constructor( logger, id, prefix, prefixSpace ) {
-    this.commands = new Commands( logger, id, prefix, prefixSpace )
-    this.filters = new Filters( logger, id )
-    this.users = new Map
-    this.invites = {}
-    this.userScope = {}
-  }
-}
+
+/**
+ * @typedef {object} GuildModule
+ * @property {object} translation
+ * @property {object} filters
+ * @property {object} commands
+ */
 
 export default class CactuDiscordBot {
-  #discordClient = new Discord.Client()
-  #botOperatorRole = ``
-  #loggedErrors = []
+  discordClient = new Discord.Client()
+  botOperatorRole = ``
+  loggedErrors = []
 
-  /** @type {Map<string,GuildData>} */
-  #guildsData = new Map()
+  /** @type {Map<string,GuildModule>} */
+  guildsData = new Map()
 
-  #moduleLogger = new Logger( [
+  moduleLogger = new Logger( [
     { align:'right',  color:'fgMagenta', length:30 }, // /(Filter|Command)/
     { align:'center', color:'bright',    length:6 },  // /(Filter|Command)/
     { align:'right',  color:'fgBlue',    length:10 }, // /(Filter|Command)/
@@ -35,26 +33,38 @@ export default class CactuDiscordBot {
     { length:3 },                                     // /:  /
     { splitLen:200, splitFLLen:150 },                 // /.*/
   ] )
-  #botLogger = new Logger( [
+  botLogger = new Logger( [
     { align:'right', color:'fgMagenta', length:5 },  // /Bot/
     { length:3 },                                    // /:  /
     { splitLen:90, splitFLLen:65 },                  // /.*/
   ] )
 
-  #prefix = `cc`
-  #prefixSpace = true
-  #evalVars = {}
-  #spamConfig = {}
-  #signs = {}
+  prefix = `cc`
+  prefixSpace = true
+  evalVars = {}
+  spamConfig = {}
+  signs = {}
 
   constructor( config ) {
-    if (!(  'prefix'      in config)) this.#prefix      = config.prefix
-    if (!(  'prefixSpace' in config)) this.#prefixSpace = config.prefixSpace
-    if (!(  'spamConfig'  in config)) this.#spamConfig  = config.spamConfig
-    if (!(  'evalVars'    in config)) this.#evalVars    = config.evalVars
-    if (!(  'signs'       in config)) this.#signs       = config.signs
+    if (!(  'prefix'      in config)) this.prefix      = config.prefix
+    if (!(  'prefixSpace' in config)) this.prefixSpace = config.prefixSpace
+    if (!(  'spamConfig'  in config)) this.spamConfig  = config.spamConfig
+    if (!(  'evalVars'    in config)) this.evalVars    = config.evalVars
+    if (!(  'signs'       in config)) this.signs       = config.signs
 
-    this.#discordClient
+    const guilds = this.guildsData
+
+    fs.readdirSync( `./guilds_modules` ).forEach( fileName => {
+      const id = fileName.match( /(.*?)-(.*)/ )[ 1 ]
+
+      if (!guilds.has( id )) guilds.set( id, [] )
+
+      import( `./guilds_modules/${fileName}` )
+        .then( module => guilds.get( id ).push( module.default ) )
+        .catch( console.log )
+    } )
+
+    this.discordClient
       .on( 'message', this.onMessage )
       .on( 'ready', this.onReady )
       .login( config.token || `` )
@@ -65,19 +75,23 @@ export default class CactuDiscordBot {
    * @param {string} string
    */
   log( string ) {
-    this.#botLogger( 'Bot', ':', string )
+    this.botLogger( 'Bot', ':', string )
   }
 
   /** New message event handler
    * @param {Discord.Message} message
    */
-  onMessage = message => {}
+  onMessage = message => {
+    const guildData = this.guildsData.get( message.guild.id )
+
+    console.log( guildData )
+  }
 
   onReady = () => {
     console.log()
     this.log( 'I have been started' )
     console.log()
 
-    this.#discordClient.user.setActivity( this.prefix, { type:'WATCHING' } )
+    this.discordClient.user.setActivity( this.prefix, { type:'WATCHING' } )
   }
 }
