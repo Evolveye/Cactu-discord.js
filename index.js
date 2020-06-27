7aaa107bdd4dc73f155e9fff28496094d17ad82e
Evolveye <stolarski.paw@gmail.com>
2020-06-27 22:01:50 +0200

    Initial command processing methods/instructions

--- index.js
+++ index.js
@@ -16,6 +16,43 @@ export const LoggerClass = Logger
  * @property {object} commands
  */
 
+class CommandData {
+  /**
+   * @param {Discord.Message} message
+   */
+  constructor( message ) {
+    const { content } = message
+
+    this.command = content.trim()
+    this.path = ''
+    this.parts = CommandData.partCommand( content )
+    this.response = { code:[], params:[], values:[] }
+    this.err = { type:null, value:null, paramMask:null }
+  }
+
+  /**
+   * @param {any} err
+   * @param {string} type
+   * @param {string} value
+   */
+  setError( err, type, value ) {
+    err.type = type
+    err.value = value
+  }
+
+  /**
+   * @param {string} command
+   */
+  static partCommand( command ) {
+    const { groups } = /^(?<part>\S+)(?: +(?<rest>[\s\S]*))?/.exec( command ) || { groups:{} }
+
+    if (!groups.part) groups.part = ''
+    if (!groups.rest) groups.rest = ''
+
+    return groups
+  }
+}
+
 export default class CactuDiscordBot {
   discordClient = new Discord.Client()
   botOperatorRole = ``
@@ -39,18 +76,18 @@ export default class CactuDiscordBot {
     { splitLen:90, splitFLLen:65 },                  // /.*/
   ] )
 
-  prefix = `cc`
+  prefix = `cc!`
   prefixSpace = true
   evalVars = {}
   spamConfig = {}
   signs = {}
 
   constructor( config ) {
-    if (!(  'prefix'      in config)) this.prefix      = config.prefix
-    if (!(  'prefixSpace' in config)) this.prefixSpace = config.prefixSpace
-    if (!(  'spamConfig'  in config)) this.spamConfig  = config.spamConfig
-    if (!(  'evalVars'    in config)) this.evalVars    = config.evalVars
-    if (!(  'signs'       in config)) this.signs       = config.signs
+    if (  'prefix'      in config) this.prefix      = config.prefix
+    if (  'prefixSpace' in config) this.prefixSpace = config.prefixSpace
+    if (  'spamConfig'  in config) this.spamConfig  = config.spamConfig
+    if (  'evalVars'    in config) this.evalVars    = config.evalVars
+    if (  'signs'       in config) this.signs       = config.signs
 
     const guilds = this.guildsData
 
@@ -78,13 +115,84 @@ export default class CactuDiscordBot {
     this.botLogger( 'Bot', ':', string )
   }
 
+  /**
+   * @param {CommandData} commandData
+   */
+  checkPrefix( commandData ) {
+    const { prefix, prefixSpace } = this
+    const { command, parts } = commandData
+
+    if (!command.startsWith( prefix )) return false
+
+    if (prefixSpace) {
+      if (prefix !== parts.part) return false
+    } else {
+      if (parts.part === prefix && parts.rest !== '') return false
+    }
+
+    if (prefixSpace) commandData.command = parts.rest
+    else commandData.command = command.slice( prefix.length )
+
+    return true
+  }
+
+  /**
+   * @param {CommandData} commandData
+   * @param {function(string[]): boolean} rolesTest
+   */
+  checkAccesToStructure( commandData, structure ) {
+    const { prefix, prefixSpace } = this
+    let { command, path, parts, err } = commandData
+
+    if (err.type) return
+
+    while ((parts = this.partCommand( command )).part !== '') {
+      if (!command) break
+
+      const { part, rest } = parts
+
+      if (!(part in structure)) {
+        this.setError( err, 'noCommand', part )
+        break
+      }
+
+      command = rest || ''
+      path += ` ${part}`
+      structure = structure[ part ]
+
+      if (structure[ '@code' ]) {
+        if (!rolesTest( structure[ '@roles' ] )) commandData.setError( err, 'badRole' )
+        break
+      } else if (!rolesTest( structure[ '@roles' ] )) {
+        commandData.setError( err, 'badRole' )
+        break
+      }
+    }
+
+    if (prefixSpace) path = `${prefix}${path}`
+    else path = `${prefix}${path.slice( 1 )}`
+
+    commandData.command = command
+    commandData.path = path
+    commandData.parts = parts
+    commandData.structure = structure
+  }
+
   /** New message event handler
    * @param {Discord.Message} message
    */
   onMessage = message => {
     const guildData = this.guildsData.get( message.guild.id )
+    const commandData = new CommandData( message )
 
-    console.log( guildData )
+    if (!this.checkPrefix( commandData )) return
+
+    // if (!err.type) this.checkAccesToStructure( commandData, structure )
+    // if (!err.type) this.buildResponse( data )
+
+    // if (err.type) this.processErrors( data )
+
+    // console.log( guildData )
   }
 
   onReady = () => {
